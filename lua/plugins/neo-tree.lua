1848b66eab97bce097475bd6bae33d5d94119b88
jai mishra <jaidev7823@gmail.com>
2026-03-02 02:14:53 +0530

    focus on sidebar when do leader e

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true, -- show filtered items
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false,
+      },
+    },
+  },
+}
